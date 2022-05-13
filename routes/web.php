3b303dcad9f49e65f9d31ccce10317168c0594ec
FabianBenc <bencfabian@hotmail.com>
2022-05-13 19:14:31 +0200

    AddedLogin,Thread Create(Crud)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThreadsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+   return view('welcome');
 });
 
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('threads',ThreadsController::class);
